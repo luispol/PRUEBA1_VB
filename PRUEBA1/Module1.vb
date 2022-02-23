3283534e6e10b457af4021cc613542759361a2bc
Luis Polanco <lumpolanco@gmail.com>
2022-02-22 22:59:11 -0600

    Agregar archivos de proyecto.

---
+++ PRUEBA1/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim estudia As Boolean = False
+
+        Console.Write("Ingrese el dinero ahorrado: ")
+        Dim dineroAhorrado As Double = Console.ReadLine()
+
+        If dineroAhorrado >= 600 Then
+            estudia = True
+        End If
+
+        If estudia = True Then
+            Console.WriteLine("El dinero ahorrado sirve para pagar la universidad")
+        Else
+            Console.WriteLine("El dinero no sirve para pagar la universidad")
+        End If
+
+
+    End Sub
+
+End Module
